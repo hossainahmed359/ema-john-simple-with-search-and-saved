00a53eb37fb42a75242190c1552f7ffb679312d3
hossainahmed359 <hossainahmed359@gmail.com>
2021-10-02 16:24:12 +0600

    Order Review functionality added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,14 +8,16 @@ const Cart = (props) => {
     // const total = cart.reduce(totalReducer, 0);
     let totalQuantity = 0;
     let total = 0;
-    for (const product of cart) {
-        if (!product.quantity) {
-            product.quantity = 1;
+    if (cart) {
+        for (const product of cart) {
+            if (!product.quantity) {
+                product.quantity = 1;
+            }
+            total = total + product.price * product.quantity;
+            totalQuantity = totalQuantity + product.quantity;
         }
-        total = total + product.price * product.quantity;
-        totalQuantity = totalQuantity + product.quantity;
-    }
 
+    }
     const shipping = total > 0 ? 15 : 0;
     const tax = (total + shipping) * 0.10;
     const grandTotal = total + shipping + tax;
